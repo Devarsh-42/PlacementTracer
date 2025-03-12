9c0ded929d9e410fc5ef28553dee8cb10e58243a
Devarsh-42 <devarshmehta2005@gmail.com>
2025-03-12 15:35:35 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <nav>
+  <a href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+</nav>
+  );
+}
+
+export default App;
